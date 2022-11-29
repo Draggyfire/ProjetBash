5140aea6d3ed68735225970b48ac79e711e1210b
Hana <clemdelcourt12@gmail.com>
2022-11-29 17:25:45 +0100

    checking triParam

--- bash.sh
+++ bash.sh
@@ -1,150 +1,159 @@
 #!/bin/bash
 
 #Tout les tris doivent prendre la même chose en paramètre : test.txt;test2.txt; (peut changer)
-#comme ça pour les tris multiple on peut appeler les mêmes fonctions 
+#comme ça pour les tris multiple on peut appeler les mêmes fonctions
 
-#ex: Tri dec prend une chaine sous la forme test.txt;test2.txt; 
-#Tri par poid prend une chaine sous la forme test.txt;test2.txt; 
-#Donc les 2 sont utilisables séparémment et si jamais les 2 sont demandés (-poid -d) et qu'il y a une égalité on peut appeller l'autre avec les égalités sous la forme fichieravecunpoid.txt;fichieraveclememepoid.txt
+#ex: Tri dec prend une chaine sous la forme test.txt;test2.txt;
+#Tri par poid prend une chaine sous la forme test.txt;test2.txt;
+#Donc les 2 sont utilisables séparémment et si jamais les 2 sont demandés (-poid -d) et qu'il y a une égalité on peut appeller l'autre avec les égalités
+#sous la forme fichieravecunpoid.txt;fichieraveclememepoid.txt
 
-function reverseAffiche(){
-    #Affiche \$2 chaines de caractere dans \$1 qui est sous la forme test.txt;test.c;test.o; 
+availableTriParam=nsmletpg
+
+function triIsAvailable() {
+    #check si la lettre correspondant au tri est possible
+    [ $# -ne 1 ] && echo "veuillez rentrez 1 parametre dans la fonction triIsAvailable !" && exit 1
+    local i
+    for ((i = 0; i < ${#availableTriParam}; i++)); do
+        [ ${availableTriParam:i:1} == "$1" ] && return 0
+    done
+    return 1
+}
+
+function reverseAffiche() {
+    #Affiche $2 chaines de caractere dans $1 qui est sous la forme test.txt;test.c;test.o;
     #de la fin au début
     var=$1
-    i=$2
-    while [ $i -ne 0 ] 
-    do
-        mot=`echo $var|cut -d';' -f$i`
-        echo $mot 
+    local i=$2
+    while [ $i -ne 0 ]; do
+        mot=$(echo $var | cut -d';' -f$i)
+        echo $mot
         var="${var%$mot;}"
-        i=$[i-1]
+        i=$((i - 1))
     done
 }
 
-function Affiche(){
-    #Affiche \$2 chaines de caractere dans \$1 qui est sous la forme test.txt;test.c;test.o; 
+function Affiche() {
+    #Affiche \$2 chaines de caractere dans $1 qui est sous la forme test.txt;test.c;test.o;
     #de la fin au début
     var=$1
-    i=$2
-    while [ $i -ne 0 ] 
-    do
-        mot=`echo $var|cut -d';' -f$i`
-        echo $mot 
+    local i=$2
+    while [ $i -ne 0 ]; do
+        mot=$(echo $var | cut -d';' -f$i)
+        echo $mot
         var="${var%$mot;}"
-        i=$[i-1]
+        i=$((i - 1))
     done
 }
 
-function triS(){
+function triS() {
     var=$1
-    i=0
-    while [ $i -ne $2 ] 
-    do
+    local i=0
+    while [ $i -ne $2 ]; do
         j=0
-        mot=`echo $var|cut -d';' -f$i`
-        echo $mot 
-        while [ $j -ne $2 ] 
-        do
-            mot=`echo $var|cut -d';' -f$i`
-            echo $mot 
+        mot=$(echo $var | cut -d';' -f$i)
+        echo $mot
+        while [ $j -ne $2 ]; do
+            mot=$(echo $var | cut -d';' -f$i)
+            echo $mot
             var="${var%$mot;}"
         done
-        i=$[i+1]
+        i=$((i + 1))
     done
 }
-function ls(){
-    #Affiche le répertoire \$1 sans le chemin donné en paramètre
-    for i in "$1"/*
-    do
+function ls() {
+    #Affiche le répertoire $1 sans le chemin donné en paramètre
+    for i in "$1"/*; do
         tmp="${i#$1/}"
         echo $tmp
     done
 }
 
-function savels(){
-    #Sauvegarde le repertoire \$1 sous la forme test.txt;test.c;
+function savels() {
+    #Sauvegarde le repertoire $1 sous la forme test.txt;test.c;
     save=""
     cpt=0
-    for i in "$1"/*
-    do
+    for i in "$1"/*; do
         tmp="${i#$1/}"
-        if [ $cpt -eq 0 ]
-        then
+        if [ $cpt -eq 0 ]; then
             save="$tmp;"
         else
             save="$save$tmp;"
         fi
-        cpt=$[cpt+1]
+        cpt=$((cpt + 1))
     done
     echo "$save $cpt"
 }
 
-function triDec(){
+function triDec() {
     res=$(savels $1)
-    nb=`echo $res|cut -d' ' -f2`
+    nb=$(echo $res | cut -d' ' -f2)
     liste="${res%$nb}"
     reverseAffiche $liste $nb
 }
 
-function lsR(){
-    for i in "$1"/*
-    do
+function lsR() {
+    for i in "$1"/*; do
         tmp="${i#$1/}"
-        for((j=0;j<$2-1;j++))
-        do
+        for ((j = 0; j < $2 - 1; j++)); do
             echo -n "|      "
         done
-        if [ $2 -ne 0 ] 
-        then
-            echo -n "|--" 
+        if [ $2 -ne 0 ]; then
+            echo -n "|--"
         fi
-        if [ -d $i ] 
-        then
-            if [ $2 -ne 0 ] 
-            then
-                echo -n ")" 
+        if [ -d $i ]; then
+            if [ $2 -ne 0 ]; then
+                echo -n ")"
             fi
             echo "$tmp"
-            var=$(($2+1))
+            var=$(($2 + 1))
             lsR $i $var
         else
-            if [ $2 -ne 0 ] 
-            then
-                echo -n ">" 
+            if [ $2 -ne 0 ]; then
+                echo -n ">"
             fi
             echo "$tmp"
         fi
     done
 }
 
-[ $# -lt 1 ] && echo "Veuillez rentrer au moins 1 argument !" && echo "$0 [-R] [-d] [-nsmletpg] rep" &&exit 1
+[ $# -lt 1 ] && echo "Veuillez rentrer au moins 1 argument !" && echo "$0 [-R] [-d] [-nsmletpg] rep" && exit 1
 [ $# -gt 4 ] && echo "Il ne peut y avloir plus de 4 arguments !" && echo "$0 [-R] [-d] [-nsmletpg] rep" && exit 1
 
 recursive=0
 decroissant=0
-if [ $1 == "-R" ] 
-then
+triParam=""
+
+if [ "$1" == "-R" ]; then
     echo "recursive 1"
     recursive=1
     shift
 fi
-if [ $1 == "-d" ] 
-then
+if [ "$1" == "-d" ]; then
     echo "décroissant 1"
     decroissant=1
     shift
 fi
-#rajouter autres parametres
-if [ $decroissant -eq 1 -a $recursive -eq 1 ]
-then
+
+#verifie l'ordre des tri a effectuer ( ne prend en compte que les tri ayant pour valeur $availableTriParam)
+if [ "${1:0:1}" == "-" ]; then
+    ThirdParamCpt=1
+    while [ $ThirdParamCpt -lt ${#1} ]; do
+        triChar=${1:ThirdParamCpt:1}
+        triIsAvailable $triChar
+        [ $? -eq 0 ] && triParam=$triParam$triChar
+        ThirdParamCpt=$((ThirdParamCpt + 1))
+    done
+    echo "tri: $triParam"
+fi
+
+if [ $decroissant -eq 1 -a $recursive -eq 1 ]; then
     echo "Les 2"
     triDec $1
-elif [ $decroissant -eq 1 ] 
-then
+elif [ $decroissant -eq 1 ]; then
     echo "tri decroissant"
     triDec $1
-elif [ $recursive -eq 1 ] 
-then
+elif [ $recursive -eq 1 ]; then
     echo "Recursive"
     lsR $1 0
 else
